4a35081275f00082ae124983cfdde661ec466660
happy-kitty0821 <ayushhaang09@gmail.com>
2024-01-28 10:11:14 +0545

    made a folder for a new project named Download Wallpaper and wrote some basic bash script functions which checks the distro'name and updates the system

---
+++ DownloadWallpaper/GetWallpaper.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#ANSI escape codes for text formatting
+BOLD="\e[1m"
+RESET="\e[0m"
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+CYAN="\e[96m"
+
+sleep 1 # sleep for 1 second
+clear # clear the terminal
+
+get_package_manager() {
+    if command -v pacman &> /dev/null; then
+        echo "Pacman"
+    elif command -v apt-get &> /dev/null; then
+        echo "APT"
+    elif command -v dnf &> /dev/null; then
+        echo "DNF"
+    elif command -v yum &> /dev/null; then
+        echo "YUM"
+    else
+        echo "Unknown"
+    fi
+}
+
+get_linux_distro_info() {
+    if [ -f /etc/os-release ]; then
+        source /etc/os-release
+        echo -e "${BOLD}${RED}[!] Distribution: ${CYAN}$NAME${RESET}"
+        echo -e "${BOLD}${YELLOW}[~] Version: ${CYAN}$VERSION${RESET}"
+        echo -e "${BOLD}${YELLOW}[~] ID: ${CYAN}$ID${RESET}"
+        echo -e "${BOLD}${YELLOW}[~] Codename: ${CYAN}$VERSION_CODENAME${RESET}"
+        echo -e "${BOLD}${YELLOW}[~] Home URL: ${CYAN}$HOME_URL${RESET}"
+        echo -e "${BOLD}${YELLOW}[~] Support URL: ${CYAN}$SUPPORT_URL${RESET}"
+        echo -e "${BOLD}${YELLOW}[~] Bug Report URL: ${CYAN}$BUG_REPORT_URL${RESET}"
+
+        #check for the package manager
+        package_manager=$(get_package_manager)
+        echo -e "${BOLD}${GREEN}[~] Package Manager: ${CYAN}$package_manager${RESET}"
+    else
+        echo -e "${BOLD}${RED}[#] Unable to determine Linux distribution information.${RESET}"
+    fi
+}
+
+updateSystem() {
+     #get the package manager
+    package_manager=$(get_package_manager)
+    sleep 1
+
+    case $package_manager in
+        "Pacman")
+            echo -e "${BOLD}${RED}[#] Updating system using Pacman.${RESET}"
+            sudo pacman -Syu
+            ;;
+        "APT")
+            echo -e "${BOLD}${RED}[#] Updating system using APT.${RESET}"
+            sudo apt update -y; sudo apt upgrade -y
+            ;;
+        "DNF")
+            echo -e "${BOLD}${RED}[#] Updating system using DNF.${RESET}"
+            sudo dnf update
+            ;;
+        "YUM")
+            echo -e "${BOLD}${RED}[#] Updating system using YUM.${RESET}"
+            sudo yum update
+            ;;
+        "Zypper")
+            echo -e "${BOLD}${RED}[#] Updating system using Zypper.${RESET}"
+            sudo zypper update
+            ;;
+        *)
+            echo -e "${BOLD}${RED}[#] Package manager not supported for updating.${RESET}"
+            ;;
+    esac
+}
+
+
+#calling of the functions
+get_linux_distro_info
+updateSystem
